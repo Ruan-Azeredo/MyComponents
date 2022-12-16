97829a58b9f9fce02b7902de6132b7f55d944854
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-16 09:16:15 -0300

    :construction: build: logica DarkModeToggle

--- pages/native/DarkModeToggle.jsx
+++ pages/native/DarkModeToggle.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react'
 import styles from '../../styles/DarkModeToggle.module.css'
 
 export default function DarkModeToggle() {
+
+    const handleToggled = () => {
+        document.body.classList.toggle("dark")
+        localStorage.setItem(
+            "dark",
+            document.body.className
+        )
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem("dark")) {
+            document.body.classList.add("dark")
+        }
+    }, [])
 
     return (
         <div>
