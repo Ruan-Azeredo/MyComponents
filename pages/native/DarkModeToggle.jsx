eeb2932421f95c181e90109cf7c7654f3a16e671
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-16 09:12:55 -0300

    :construction: build: referencia style.module usa

--- pages/native/DarkModeToggle.jsx
+++ pages/native/DarkModeToggle.jsx
@@ -1,5 +1,10 @@
+import styles from '../../styles/DarkModeToggle.module.css'
+
 export default function DarkModeToggle() {
+
     return (
-        
+        <div>
+            <div className={styles.oi}>oiiiiii</div>
+        </div>
     )
 }
