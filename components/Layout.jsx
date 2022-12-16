2300b4da5d12c0f79fd97bddb52dddd5dcbb5711
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-16 07:55:21 -0300

    :recycle: refactor: refaz cores layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 export default function Layout(props) {
     return (
-        <section className={`bg-green-500 h-screen w-screen flex justify-center items-center
+        <section className={`bg-gradient-to-r from-[#13547A] to-[#639993] h-screen w-screen flex justify-center items-center
         p-20`}>
-            <div className="bg-slate-500 h-full w-full flex justify-center items-center rounded-3xl">
+            <div className="bg-white h-full w-full flex justify-center items-center rounded-3xl">
                 {props.children}
             </div>
         </section>
